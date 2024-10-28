0a376faf80989c519f107cb716d1248f39ae1f57
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-10-28 17:38:34 +0530

    links added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,10 +4,11 @@ import React, { useEffect, useRef, useState } from "react";
 import menu_close from "./images/close.svg";
 import open_menu from "./images/menu.svg";
 import home_icon from "./images/home.svg";
-import about from './images/about.svg';
-import notes from './images/notes.svg';
-import contact from './images/contacts.svg';
+import about from "./images/about.svg";
+import notes from "./images/notes.svg";
+import contact from "./images/contacts.svg";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   // gsap.registerPlugin(TextPlugin);
@@ -47,62 +48,63 @@ export default function Navbar() {
       <header>
         <nav className="d-flex justify-content-between align-items-center">
           <ul className="sidebar">
-            <li onClick={closeSidebar}>
-              <a href="#" className="gap-3">
-                <img src={menu_close} alt="close" className="sidebyside" />
-                Close
-              </a>
+            <li onClick={closeSidebar} className="gap-3">
+              <img src={menu_close} alt="close" className="sidebyside" />
+              Close
             </li>
             <li>
-              <a href="/" className="gap-3">
+              <Link to="/" className="gap-3" onClick={closeSidebar}>
                 <img src={home_icon} alt="home" className="sidebyside" />
                 Home
-              </a>
-            </li >
+              </Link>
+            </li>
             <li>
-              <a href="/" className="gap-3">
+              <Link to="/notes" className="gap-3" onClick={closeSidebar}>
                 <img src={notes} alt="about" className="sidebyside" />
                 Notes
-                </a>
+              </Link>
             </li>
             <li>
-              <a href="/" className="gap-3">
+              <Link to="/about" className="gap-3" onClick={closeSidebar}>
                 <img src={about} alt="about" className="sidebyside" />
                 About
-                </a>
+              </Link>
             </li>
             <li>
-              <a href="/" className="gap-3">
+              <Link to="/contact" className="gap-3" onClick={closeSidebar}>
                 <img src={contact} alt="about" className="sidebyside" />
-                Contact Me</a>
+                Contact Me
+              </Link>
             </li>
-            <button className='btn btn-dark lastelement'>Sign Up</button>
+            <button className="btn btn-dark lastelement">Sign Up</button>
           </ul>
           <div className="main-logo">
             <span className="myName">iNOTES</span>
           </div>
           <div className="li mr-auto">
-            <a href="/" className="hideOnMobile">
+            <Link to="/" className="hideOnMobile">
               Home
-            </a>
-            <a href="/" className="hideOnMobile">
+            </Link>
+            <Link to="/notes" className="hideOnMobile">
               Notes
-            </a>
-            <a href="/" className="hideOnMobile">
+            </Link>
+            <Link to="/about" className="hideOnMobile">
               About
-            </a>
-            <a href="/" className="hideOnMobile">
+            </Link>
+            <Link to="/contact" className="hideOnMobile">
               Contact Me
-            </a>
+            </Link>
           </div>
           <div>
-            <button className='btn btn-dark lastspan hideOnMobile'>Sign Up</button>
+            <button className="btn btn-dark lastspan hideOnMobile">
+              Sign Up
+            </button>
           </div>
-            <li className="menu-btn" onClick={showSidebar}>
-              <a href="#">
-                <img src={open_menu} alt="menu" />
-              </a>
-            </li>
+          <li className="menu-btn" onClick={showSidebar}>
+            <a href="#">
+              <img src={open_menu} alt="menu" />
+            </a>
+          </li>
         </nav>
       </header>
     </>
